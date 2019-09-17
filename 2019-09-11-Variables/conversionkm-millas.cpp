67a9ff5055e42c4a4e186692869a243f83a785e2
dimunozv1 <54986688+dimunozv1@users.noreply.github.com>
2019-09-16 22:51:21 -0500

    Add files via upload

--- 2019-09-11-Variables/conversionkm-millas.cpp
+++ 2019-09-11-Variables/conversionkm-millas.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <cmath>
 
 int main (void)
 {
-  double miles = 0.0;
-  double km = 0.0;
-  std::cout << "Enter number of km: "<< '\n';
-  miles = 1.6 km;
-  std::cout << "Number in miles: " <<  << '\n';
-
+  double x = 0.0;
+  std::cout << "Enter number of miles : "<< std::endl ;
+  std::cin >> x ;
+  std::cout << "that is : " << x*1.609 << " km" << std::endl;
+  
+  return 0;
 }
 
