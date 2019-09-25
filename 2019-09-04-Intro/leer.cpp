4b3a47494a57f9f04fa3af0f3c7d0e1d4d9606ac
Diana del Mar Munoz Valencia <dimunozv@darkstar.org>
2019-09-25 08:39:06 -0500

    intro

---
+++ 2019-09-04-Intro/leer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+
+{ 
+double x = 0.0;
+
+std::cout << "Escribe un numero" << std::endl;
+std::cin >> x  ;
+std::cout << "Hola, he leido: " << x << std::endl ;
+return 0;}
+
