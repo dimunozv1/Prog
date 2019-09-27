b35fc98e816810c1431be1daf8cde7a5d6f6300a
Diana del Mar Munoz Valencia <dimunozv@darkstar.org>
2019-09-27 08:02:54 -0500

    error: con int, double devuelve un double si es el numero maximo

--- 2019-09-27-Functions-II/template-max.cpp
+++ 2019-09-27-Functions-II/template-max.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 
-template <typename T>
-T max (T a, T b)
+template <typename T, typename S>
+T max (T a, S b)
 
 {
   T result = 0;
@@ -20,6 +20,8 @@ int main (void)
 
   std::cout << max (-35,2) << std::endl;
   std::cout << max (-35.87,2.98) << std::endl;
+  std::cout << max (-35.8,2) << std::endl;
+  std::cout << max (-35,2.9) << std::endl;
 
   return 0;
 }
