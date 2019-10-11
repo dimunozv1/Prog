90c0b949be3b32d786eba5044b700651b709fc0b
Diana del Mar Munoz Valencia <dimunozv@darkstar.org>
2019-10-11 08:53:42 -0500

    corregir el codigo

---
+++ 2019-10-11-root_finding/biseccion.cpp
@@ -0,0 +1,48 @@
+#include <cmath>
+#include <iostream>
+
+using fptr = double (double);
+
+double biseccion (double xl, double xu, double eps, fptr fun);
+double funcion (double x);
+
+int main(void)
+{
+  std::cout.precision(15); std::cout.setf(std::ios::scientific);
+
+  const double eps = 1.0e-6;
+  double root = biseccion(100, 200, eps, funcion);
+  std::cout << root << "\n";
+
+  
+
+  return 0;
+}
+
+double biseccion (double xl, double xu, double eps, fptr fun)
+{
+  for(fun(xl)*fun(xu) < 0;
+    {
+    double xr = (xl + xu)/2;
+    if(std::fabs(f(xr)) <= eps){
+      
+      break ;
+    }
+    else {
+      if(fun(xl)*fun(xr) < 0){
+	xu = xr;
+      }
+      if(fun(xl)*fun(xr) > 0){
+	xl = xr;
+      }
+    }
+  }
+  return xr;
+}
+
+double funcion (double x)
+{
+  return ((x*std::cosh(50.0/x))-x-10.0);
+}
+
+      
