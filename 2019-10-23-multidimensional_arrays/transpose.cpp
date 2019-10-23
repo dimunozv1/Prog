3179c1a782a19ef55d805e6aa8de8018fa983216
Diana del Mar Munoz Valencia <dimunozv@darkstar.org>
2019-10-23 08:12:55 -0500

    Acabar los codigos

---
+++ 2019-10-23-multidimensional_arrays/transpose.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+const int M = 5, N = 5;
+void print(int C[M][N],int m, int n);
+
+int main(void){
+  //declarar las matrices
+
+  const int M = 5, N = 5;
+    int A[M][N]={{0}};
+    int AT[N][M]={{0}};
+    int ii = 0;
+    int jj = 0;
+    
+    for(ii=0;ii<M;ii++){
+      for(jj=0;jj<N;jj++)
+	{ A[ii][jj]= ii*N+jj;
+	  
+	   }
+      }
+    
+    std::cout <<"Original Matrix " << "\n";
+    for(ii=0;ii<M;ii++){
+      for(jj=0;jj<N;jj++)
+	{ 
+	  std::cout << A[ii][jj]<< "\t ";
+	   }
+      std::cout << "\n";}
+
+  //procesarla :transponerla
+     for(ii=0;ii<M;ii++){
+       for(jj=0;jj<N;jj++){
+	 AT[jj][ii] = A[ii][jj];
+	 
+       }
+
+     }
+		       
+  //imprimirla
+     std::cout << "Transposed Matrix " << "\n";
+      for(ii=0; ii<N ;ii++){
+	for(jj=0; jj<M ;jj++){ 
+	  std::cout << AT[ii][jj]<< "\t ";
+	   }
+      std::cout << "\n";}
+
+  return 0;
+}
